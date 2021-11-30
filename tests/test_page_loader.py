00a9cf346a03a7e28e770f8701204e726e1c7646
Igor_Aukhadeev <rumbks@gmail.com>
2021-11-30 12:35:53 +0500

    Add negative tests

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -1,7 +1,9 @@
+import os
 import tempfile
 from pathlib import Path
 
 import pytest
+import requests
 from funcy import ilen
 from pytest import fixture
 
@@ -19,6 +21,11 @@ PAGE_URL = "https://ru.hexlet.io/courses"
 IMAGE_URL = "https://ru.hexlet.io/assets/image.jpeg"
 CSS_URL = "https://ru.hexlet.io/assets/application.css"
 SCRIPT_URL = "https://ru.hexlet.io/packs/js/runtime.js"
+
+URL_STATUS_400 = "https://ru.hexlet.io/400"
+URL_STATUS_500 = "https://ru.hexlet.io/500"
+URL_TIMEOUT = "https://ru.hexlet.io/timeout"
+ERROR_REASON = 'reason'
 
 EXPECTED_RESOURCES_DIR = "ru-hexlet-io-courses_files"
 
@@ -49,10 +56,8 @@ def set_up_requests_mocks(requests_mock):
     requests_mock.get(SCRIPT_URL, text=get_content(SCRIPT))
 
 
-def test_successful_download(
-    set_up_requests_mocks,
-    temporary_directory,
-):
+@pytest.mark.usefixtures('set_up_requests_mocks')
+def test_successful_download(temporary_directory):
     with temporary_directory as directory:
         path_to_page = Path(download(PAGE_URL, directory))
 
@@ -66,7 +71,8 @@ def test_successful_download(
             assert is_same(resources_dir / resource_filename, expected)
 
 
-def test_error_nonexistent_directory_to_save(set_up_requests_mocks):
+@pytest.mark.usefixtures('set_up_requests_mocks')
+def test_error_nonexistent_directory_to_save():
     nonexistent_directory = "/nonexistent"
     with pytest.raises(RuntimeError) as error:
         download(PAGE_URL, nonexistent_directory)
@@ -74,3 +80,43 @@ def test_error_nonexistent_directory_to_save(set_up_requests_mocks):
             str(error.value)
             == f"Directory {nonexistent_directory} does not exists."
         )
+
+
+@fixture
+def set_up_requests_mocks_errors(requests_mock):
+    requests_mock.get(URL_STATUS_400, status_code=400, reason=ERROR_REASON)
+    requests_mock.get(URL_STATUS_500, status_code=500, reason=ERROR_REASON)
+    requests_mock.get(URL_TIMEOUT, exc=requests.exceptions.Timeout(ERROR_REASON))
+
+
+@pytest.mark.parametrize(
+    ('url', 'exception', 'message'),
+    [
+        (
+            URL_STATUS_400,
+            requests.HTTPError,
+            f'400 Client Error: {ERROR_REASON} for url: {URL_STATUS_400}',
+        ),
+        (
+            URL_STATUS_500,
+            requests.HTTPError,
+            f'500 Server Error: {ERROR_REASON} for url: {URL_STATUS_500}',
+        ),
+        (URL_TIMEOUT, requests.Timeout, ERROR_REASON),
+    ],
+)
+@pytest.mark.usefixtures('set_up_requests_mocks_errors')
+def test_http_error(temporary_directory, url, exception, message, caplog):
+    with temporary_directory as directory:
+        with pytest.raises(exception) as error:
+            download(url, directory)
+        assert str(error.value) == message
+
+@pytest.mark.usefixtures('set_up_requests_mocks')
+def test_file_write_error(temporary_directory):
+    with temporary_directory as directory:
+        os.chmod(directory, 0o444)
+        with pytest.raises(PermissionError) as error:
+            download(PAGE_URL, directory)
+        assert "Permission denied" in str(error.value)
+
